fc01a2aca2cb11d581624bc6063858d15127b613
hemanth <hemanth7787@gmail.com>
2016-07-15 19:51:08 +0530

    Python middleman

---
+++ python/pyredis.py
@@ -0,0 +1,65 @@
+import redis
+import paho.mqtt.client as mqttP
+import json
+
+
+#REDIS ***********************
+def my_handler(message):
+    print 'MY HANDLER: ', message['data']
+    data = json.loads(message['data'])
+    value = data.get('dir')
+    if value:
+        data.pop('dir',None)
+        mqtt_pub(json.dumps(data))
+
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+p = r.pubsub()
+p.subscribe(**{'mqtt-channel': my_handler})
+thread = p.run_in_thread(sleep_time=0.001)
+#t=p.get_message()
+#print(t)
+
+def redis_pub(data):
+    r.publish('mqtt-channel', data)
+
+
+#MQTT*************************
+import paho.mqtt.client as mqtt
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("DEV/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    redis_pub(msg.payload)
+
+
+
+client = mqtt.Client()
+client.username_pw_set('su1','1234')
+client.on_connect = on_connect
+client.on_message = on_message
+
+
+def mqtt_pub(data):
+    print 'PUBLISHING TO SERVER MQTT'
+    client.publish('DEV/board1/device1',data)
+
+
+client.connect("127.0.0.1", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
+
+
